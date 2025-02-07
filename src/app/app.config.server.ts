42666af4f1b18f7fed6878c556d8486f109fd8c7
Konsing <ham.konsing@gmail.com>
2025-02-06 16:28:09 -0800

    Deleted old files

--- src/app/app.config.server.ts
+++
@@ -1,9 +0,0 @@
-import { ApplicationConfig } from '@angular/core';
-import { provideHttpClient } from '@angular/common/http';
-
-export const config: ApplicationConfig = {
-  providers: [
-    provideHttpClient(),
-    // Add other providers here if needed
-  ],
-};
