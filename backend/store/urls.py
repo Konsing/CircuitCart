5accfd167df340f2fc6316de591b8ffc4b0a4349
Konsing <ham.konsing@gmail.com>
2025-02-08 20:58:53 -0800

    Fixed url files in store/ and ecommerce/

--- backend/store/urls.py
+++ backend/store/urls.py
@@ -1,18 +1,9 @@
 # store/urls.py
 from django.urls import path
-from .views import ProductList, FeaturedProductList, ComboProductList
+from . import views
 
 urlpatterns = [
-    path('products/', ProductList.as_view(), name='product-list'),
-    path('products/featured/', FeaturedProductList.as_view(), name='featured-product-list'),
-    path('products/combos/', ComboProductList.as_view(), name='combo-product-list'),
-]
-
-# ecommerce/urls.py
-from django.contrib import admin
-from django.urls import path, include
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('api/', include('store.urls')),  # API endpoints will be available under /api/
+    path('products/', views.ProductList.as_view(), name='product-list'),
+    path('products/featured/', views.FeaturedProductList.as_view(), name='featured-product-list'),
+    path('products/combos/', views.ComboProductList.as_view(), name='combo-product-list'),
 ]
